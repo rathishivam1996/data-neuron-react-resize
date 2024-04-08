07f0d39d3f06316304b08ca74e0f0fc81cb9d919
Shivam Rathi <shivam.rathi@hokmakeup.com>
2024-04-08 08:29:02 +0530

    test

--- src/use-resizable/card.jsx
+++ src/use-resizable/card.jsx
@@ -64,10 +64,14 @@ const Card = () => {
 
       <div className="card-summary-wrapper">
         <p className="card-summary">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-          repellendus nesciunt fugiat fuga odio iure saepe accusantium
-          blanditiis consequuntur nemo impedit autem, cumque voluptatum magni
-          distinctio reprehenderit ea dignissimos maxime.
+          I am a Full stack developer using Java with over 2 years of
+          experience. I have developed a strong foundation in creating and
+          maintaining robust and scalable web applications. I am skilled in
+          using Spring Boot to develop RESTful APIs and implementing
+          microservices architecture. I am a Data Engineering on Microsoft Azure
+          (DP-203) & Microsoft Azure Fundamentals (AZ-900) certified
+          professional. I work well under pressure and consistently meet
+          deadlines and targets while delivering high-quality work. Skills
         </p>
       </div>
 
@@ -101,16 +105,61 @@ const Card = () => {
       >
         <div className="card-detail-inner">
           <p className="card-detail">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            quis numquam rem earum dolores vero quia reiciendis sint, saepe iste
-            deserunt perferendis a molestias possimus culpa impedit. Deleniti,
-            nam vitae! Dolorum nam iure sint fuga ad in eius culpa quae.
-            Voluptatum quod fugiat maiores ipsam asperiores, similique numquam
-            facere quia cupiditate, quae earum, repellat autem facilis cum eius
-            obcaecati architecto. Est, quia aliquid! Molestiae est suscipit
-            nostrum vero, voluptate recusandae totam, eius voluptatum facere
-            quasi quaerat dignissimos consequuntur sunt aspernatur fugit vel
-            optio nihil ex ea earum expedita laudantium! Dolores.
+            Java • Spring Boot • Spring Data JPA • Java Database Connectivity
+            (JDBC) • Hibernate • Spring Security • MySQL • Git • CI/CD • HTML •
+            CSS • React.js • Next.js • Tailwind CSS • Shopify Python • Microsoft
+            Azure • Azure Databricks • Azure Machine Learning Full Stack
+            <h2>Developer, HOK Beauty Pvt Ltd, Dec 2023</h2> –{' '}
+            <p>
+              Present Applied expertise in web development through hands-on
+              experience in e-commerce projects. Successfully implemented key
+              functionalities, including quick view and add-to-cart features,
+              enhancing user experience Collaborated on the integration of
+              Indian Logistics Services and clients like Ekart and Delhivery to
+              streamline order fulfillment processes. Proven proficiency in
+              crafting bespoke Shopify themes tailored to meet specific client
+              needs. Extensive experience leveraging Shopify APIs to develop
+              feature-rich and customized e-commerce websites. Successfully
+              integrated third-party services and optimized workflows to ensure
+              seamless e-commerce operations.
+            </p>
+            <h2>Programmer Analyst, Cognizant, Oct 2022 – Nov 2023 (1 year)</h2>
+            Connect to Data Sources Oracle SQL and perform operations using
+            Spring JDBC, Spring Data JPA, and Spring ORM. Configure POJOs with
+            backend tables in Hibernate to achieve the associations. Develop
+            code using Design patterns like DAO, Factory Pattern, Singleton, and
+            Microservice Architecture. Facilitated seamless online transactions
+            by integrating Razorpay payment gateway for secure and efficient
+            payment processing. Configured and registered REST services with
+            Netflix Eureka for service discovery. Participated in code reviews
+            and provided feedback to peers to ensure high quality, maintainable
+            code. Got an opportunity to learn Spring Security, Basic
+            Authentication, OAuth 2.0, OpenID Connect, and JWT tokens. Full
+            Stack Developer (intern), Cognizant, Feb 2022 – Aug 2022 (6 Months)
+            Implemented global exception handling and logging for Rest
+            Controllers using SLF4J. Conducted extensive testing and debugging
+            on Spring Boot application, resulting in an increased unit test
+            coverage from 55% to 90%. Used Swagger 2.0 to describe and document
+            RESTful APIs. Used Postman to design, build, test, and iterate APIs.
+            Experience in Version control (git) and Code Repository (GitHub).
+            Licenses & Certifications Microsoft Certified: Azure Data Engineer
+            Associate - Microsoft Issued: Jun 2023 – Expires: Jun 2024
+            Certification Id: I802-3769 Skills: Azure Data Lake Storage, Azure
+            Databricks, Azure Data Factory, Azure Synapse Analytics, Azure
+            Stream Analytics AWS Certified Machine Learning - Specialty - Udemy
+            Issued: Jun 2023 Agile Fundamentals: Including Scrum & Kanban -
+            Udemy Issued: Jun 2023 Microsoft Certified: Azure Fundamentals -
+            Microsoft Issued: Mar 2022 Certification Id: I789-0779 Skills: Cloud
+            concepts, Azure management and governance, Azure architecture and
+            services Education Guru Gobind Singh Indraprastha University Master
+            of Technology - MTech, Information Technology, Sep 2020 – Aug 2022 I
+            have completed my master’s degree from USICT, GGSIPU in the field of
+            Information Technology. I have done my major project in the field of
+            Artificial Intelligence and Neural Networks. KIIT College of Engg.
+            Sohna Road, Bhondsi (Gurgaon) Bachelor of Technology - BTech,
+            Electronics and Communications Engineering, Aug 2014 - Aug 2018 I
+            have completed my bachelor’s degree in ECE from KIIT College of
+            Engineering.
           </p>
         </div>
       </div>
